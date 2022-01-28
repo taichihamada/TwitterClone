be013bc75797ab1d91b649cecdd78fcc86bb6eca
taichi <taichi.hamada514@gmail.com>
2022-01-28 22:37:31 +0900

    ログイン機能

--- config.php
+++ config.php
@@ -4,4 +4,9 @@ ini_set('display_errors',1);
 //日本時間にする
 date_default_timezone_set('Asia/Tokyo');
 //URL/ディレクトリ設定
-define('HOME_URL','http://localhost/TwitterClone/');
+define('HOME_URL','/TwitterClone/');
+//データベースの接続情報
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','root');
+define('DB_NAME','twitter_clone');
